3cf1b95b194a43d6348d03f280c655f6b2d00301
Luigi <ciriello.luigi7@outlook.it>
2025-10-09 17:50:11 +0200

    Add eureka server module

---
+++ eurekaserver/src/test/java/com/luigiciriello/ecommerce/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luigiciriello.ecommerce.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
